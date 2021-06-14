ce76fab025f1cb8e77c83e3cd8b1f82e7a4b2e7f
Moriah Taylor <moriahataylor@gmail.com>
2021-06-14 19:57:26 -0400

    update js file

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,8 @@
 //USE D3 LIBRARY TO READ IN JSON FILE//
 
 function init() {
-    d3.json("./samples.json").then(function(data){
-        var values = data.sample_values;
-        var ids = data.otu_ids;
-        var labels = data.otu_labels;
-        console.log(values);
-        console.log(ids);
-        console.log(labels);
+    d3.json("samples.json").then(function(data){
+        console.log(data);
     })
 }
 
