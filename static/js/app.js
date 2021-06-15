b30f1b7c0fe2474981a8ece13d2322a16c3c6250
Moriah Taylor <moriahataylor@gmail.com>
2021-06-15 17:58:30 -0400

    create dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,16 @@
 //USE D3 LIBRARY TO READ IN JSON FILE//
 
 function init() {
-    d3.json("samples.json").then(function(data){
-        console.log(data);
+    //select HTML dropdown element
+    var dropdown = d3.select("#selDataset")
+    //get data from json file
+    d3.json("./samples.json").then(function(data){
+        //append sample id numbers to dropdown menu
+        data.names.forEach((sample) => {
+            dropdown.append("option")
+                    .text(sample)
+                    .property("value", sample)
+        })
     })
 }
 
