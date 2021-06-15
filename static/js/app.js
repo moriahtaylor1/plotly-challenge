daff306caf8f5e8af9f339292188b6430f5ca859
Moriah Taylor <moriahataylor@gmail.com>
2021-06-15 18:23:19 -0400

    create a bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -10,11 +10,11 @@ function init() {
             dropdown.append("option")
                     .text(sample)
                     .property("value", sample)
-        })
+        });
     //create initial data that appears on first load of page
-    var initial = data.samples[0]    //pick first sample
+    var initial = data.samples[0];    //pick first sample
     //create bar chart
-    //INSERT CODE HERE
+    graphBar(initial);
     //create bubble chart
     //INSERT CODE HERE
     //show metadata
@@ -25,5 +25,26 @@ function init() {
 init();
 
 //CREATE A HORIZONTAL BAR CHART TO DISPLAY TOP 10 OTUs FOUND IN INDIVIDUAL//
+function graphBar(data){
+    //create trace
+    var trace = {
+        x: data.sample_values,
+        y: data.otu_ids,
+        type: 'bar'
+    };
+    
+    var graphData = [trace];
+
+    //stylize
+    var layout = {
+        title: "Title",
+        xaxis: { title: "X-Axis"},
+        yaxis: {title: "Y-Axis"}
+    };
+
+    //plot chart to a div tag with id "bar"
+    Plotly.newPlot("bar", graphData, layout)
+    
+}
 
 //CREATE A BUBBLE CHART THAT DISPLAYS EACH SAMPLE
